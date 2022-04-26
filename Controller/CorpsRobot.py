1fa85c09181fa8350c1aab99d9cb44aa07866cac
Rom9281 <34047259+Rom9281@users.noreply.github.com>
2022-04-26 18:40:48 +0200

    Travail sur le corps du robot: changement serializer

--- Controller/CorpsRobot.py
+++ Controller/CorpsRobot.py
@@ -12,31 +12,45 @@ Description :  Modélisation du robot en entier
 import sys, json
 from setuptools import Command
 
-from Controller.Vitesse import Vitesse
-from Controller.Commande import Commande
+from Controller.Enums import Vitesse, Commande,Sens
+
+# from Model.Serializer import Serializer
+from Model.STM import STM
+from Model.Lidar import Lidar
+from Model.ServoMoteur import ServoMoteur
 
 import Model.Serializer as Serializer
 
 class CorpsRobot():
     
     def __init__(self):
+        self.__config_periph_path = "/home/pi/Documents/Controller/configPeriph.json"
         
-        self.__config_periph_path = "/home/pi/Documents/Controller/configPeriph.json" # Fichier permettant d'indiquer quelle peripheriques sont branches 
         self.__vitesse = Vitesse.RAPIDE # Definit la vitesse initiale comme lente
-        self.__config_periph = json.load(open(self.__config_periph_path)) # récupère la config des periphériques dans le json
-        
-        # CONFIGURATION DES ELEMENTS DU CORPS
-        # ___________________________________
 
-        self.__serializer = Serializer.Serializer(
+        self.__config_periph = json.load(open(self.__config_periph_path)) # récupère la config des periphériques dans le json
+
+        """
+        self.__serializer = Serializer(
             self.__config_periph["Serializer"]["Pin"],
-            self.__config_periph["Serializer"]["Baud"]
-            ) # Configure le Serializer comme voulut
-        
+            self.__config_periph["Serializer"]["Baud"]) # Configure le Serializer comme voulut
+        """
+
+        self.__stm = STM(
+            self.__config_periph["STM32"]["Pin"],
+            self.__config_periph["STM32"]["Baud"])
+
+        self.__lidar = Lidar(
+            self.__config_periph["Lidar"]["Pin"],
+            self.__config_periph["Lidar"]["Baud"]
+        )
+
+        self.__servo_moteur = ServoMoteur(self.__stm)
+    
     """"
     Prend une distance en metre et envois la commande au Serializer
     NOTE : Distance en cm, vitesse comme décrite dans le enum vitesse
-    """
+    
     def moveForward(self,distance):
         self.__serializer.actionner(
             {
@@ -47,5 +61,22 @@ class CorpsRobot():
                 }
             }
         )
-        
     
+    @PARAM:
+     - coté : enum Sens
+     - Angle en degré
+    
+    def turn(self,sens,angle):
+        self.__serializer.actionner(
+            {
+                "commande":Commande.TOURNER,
+                "param": {
+                    "vitesse":self.__vitesse,
+                    "angle":angle,
+                    "sens":sens,
+                }
+            }
+        )
+    """
+        
+
