864e79d0457756fb20d79225d5087e001ca3591d
Emmixe <72150765+Emmixe@users.noreply.github.com>
2022-04-27 11:41:52 +0200

    Update testmax.py

--- testmax.py
+++ testmax.py
@@ -1,4 +1,8 @@
 from rplidar import RPLidar
+import time
+coord_init=[0,0]
+coord_actuelle=[0,0]
+orientation_actuelle=0
 
 def obstacle_avant (m) : 
     ret = False
@@ -33,9 +37,70 @@ def obstacle_droite (m) :
                 if t[2]<= 55:
                     ret = True
     return ret
+    
+def virage_droite():
+    orientation(1)
+    print('virage droite')
+    return "digo 1:550:70 2:-550:70\r"
+
+def virage_gauche():
+    orientation(-1)
+    print('virage gauche')
+    return "digo 1:-550:70 2:550:70\r"
+
+def avancer():
+    global orientation_actuelle
+    if orientation_actuelle == 0:
+        coord_actuelle[1]+=1
+    elif orientation_actuelle == 1:
+        coord_actuelle[0]+=1
+    elif orientation_actuelle ==2 :
+        coord_actuelle[1]-=1
+    else :
+        coord_actuelle[0]-=1
+    print('avance')
+    time.sleep(0.5)
+    return "mogo 1:-30 2:-30\r"
+
+def reculer():
+    global orientation_actuelle
+    if orientation_actuelle == 0:
+        coord_actuelle[1]-=1
+    elif orientation_actuelle == 1:
+        coord_actuelle[0]-=1
+    elif orientation_actuelle ==2 :
+        coord_actuelle[1]+=1
+    else :
+        coord_actuelle[0]+=1
+    print('recule')
+    time.sleep(0.5)
+    return "mogo 1:30 2:30\r"
+
+def orientation(p):
+    global orientation_actuelle
+    if orientation_actuelle==3 and p==1:
+       orientation_actuelle = 0
+    elif orientation_actuelle==0 and p==-1:
+        orientation_actuelle = 3
+    else :
+         orientation_actuelle += p
+    return
+
+def premier_tour():
+    avancer()
+    while coord_actuelle != coord_init :
+        if obstacle_gauche(lidar.iter_scans()[-1]):
+            if obstacle_avant(lidar.iter_scans()[-1]):
+                virage_droite()
+                avancer()
+            else :
+                avancer()
+        else:
+            virage_gauche()
+            avancer()
+        print(coord_actuelle)
+    return 
 
 lidar = RPLidar('/dev/ttyUSB0')
-for i, scan in enumerate(lidar.iter_scans()):
-    print(obstacle_avant(scan))
-
+premier_tour()
 
