4f0a5428950e9e473ef988cf4f5a161c226922fa
Emmixe <72150765+Emmixe@users.noreply.github.com>
2022-05-04 10:54:11 +0200

    Update appel_lidar.py

--- pt/appel_lidar.py
+++ pt/appel_lidar.py
@@ -3,6 +3,7 @@ import fonction_obstacles as fo
 
 while (1):
     scan = fl.scan()
+    sacn=scan[0]
     print(fo.obstacle_avant(scan))
     print(fo.obstacle_droite(scan))
     print(fo.obstacle_gauche(scan))
